959671506abc51defa23225ac664d3788dd06a3c
AntonVTR <zagco@ya.ru>
2020-03-29 23:35:06 +1000

    trying move initialization in module

---
+++ tests/elements.js
@@ -0,0 +1,86 @@
+const wdio = require("webdriverio");
+const { opts } = require('./settings');
+
+var driver;
+
+// MyModule.prototype.someFunction = function(dr) {
+//     this.driver = dr;
+// }
+
+
+async function init() {
+    driver = await wdio.remote(opts);
+
+    exports.fd1 = await driver.findElement('accessibility id', 'inputFieldLeft');
+    exports.fd2 = await driver.findElement('accessibility id', 'inputFieldRight');
+    exports.add = await driver.findElement('accessibility id', 'additionButton');
+    exports.sub = await driver.findElement('accessibility id', 'subtractButton');
+    exports.mult = await driver.findElement('accessibility id', 'multiplicationButton');
+    exports.div = await driver.findElement('accessibility id', 'divisionButton');
+    exports.rst = await driver.findElement('accessibility id', 'resetButton');
+    exports.res = await driver.findElement('accessibility id', 'resultTextView');
+}
+
+// bad practice, but element.click() and others doesn't work in Android emulator 
+async function tapSum() {
+    await driver.touchPerform([
+        { action: 'press', options: { x: 116, y: 236 } },
+        { action: 'release' }
+    ]);
+}
+async function tapSub() {
+    await driver.touchPerform([
+        { action: 'press', options: { x: 258, y: 236 } },
+        { action: 'release' }
+    ]);
+}
+
+async function tapMult() {
+    await driver.touchPerform([
+        { action: 'press', options: { x: 394, y: 236 } },
+        { action: 'release' }
+    ]);
+}
+
+function tapDiv() {
+    await driver.touchPerform([
+        { action: 'press', options: { x: 507, y: 236 } },
+        { action: 'release' }
+    ]);
+}
+
+function tapRst() {
+    await driver.touchPerform([
+        { action: 'press', options: { x: 660, y: 236 } },
+        { action: 'release' }
+    ]);
+}
+
+async function setCheckText(el, text) {
+    driver.elementSendKeys(el.ELEMENT, text);
+    await driver.getElementAttribute(el.ELEMENT, 'text').then((attr) => {
+        assert.equal(attr, text);
+    });
+}
+async function validateText(el, text) {
+    await driver.getElementAttribute(el.ELEMENT, 'text').then((attr) => {
+        assert.equal(attr, text);
+    });
+}
+async function validateTextNotEqual(el, text) {
+    await driver.getElementAttribute(el.ELEMENT, 'text').then((attr) => {
+        assert.notEqual(attr, text);
+    });
+}
+module.exports.init = init;
+module.exports.tapSum = tapSum;
+module.exports.tapDiv = tapDiv;
+module.exports.tapMult = tapMult;
+module.exports.tapSub = tapSub;
+module.exports.tapRst = tapRst;
+module.exports.setCheckText = setCheckText;
+module.exports.validateText = validateText;
+module.exports.validateTextNotEqual = validateTextNotEqual;
+
+
+exports.driver = driver;
